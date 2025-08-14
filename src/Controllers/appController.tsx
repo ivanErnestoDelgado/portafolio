a5b8fc2a27420ae191af9c08f4f486c6467ac7f0
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-08-14 13:16:22 -0700

    AppController added for user events managment and unify the views and models

---
+++ src/Controllers/appController.tsx
@@ -0,0 +1,18 @@
+import { App } from '../Views/App';
+import {Project} from '../Models';
+ 
+
+export const AppController = () => {
+    
+    const projects=[
+      new Project("Task Manager", "Una API rest de un gestor de tareas hecha con Express.js","https://github.com/ivanErnestoDelgado/TaskProjectBackend"),
+      new Project("Michican","Una pagina web de una veterinaria diseñada en JSP","https://github.com/ivanErnestoDelgado/Michican"),
+      new Project("GrowSmart API","Una API rest hecha en Django para el control de datos de riego de diferentes macetas","https://github.com/ivanErnestoDelgado/GrowSmartServidor"),
+      new Project("Loco Momo","Un proyecto en C# que consiste en recrear el juego de mesa Loco Momo","https://github.com/ivanErnestoDelgado/LocoMomo")
+    ];
+    
+
+  return (
+    <App projects={projects} />
+  )
+}
