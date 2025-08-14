8a2f67e901a38fa86891ff9c5487773c8bb54023
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-08-14 13:12:00 -0700

    Project model and index file implemented in models folder because a MVC design pattern implementation

---
+++ src/Models/projects.ts
@@ -0,0 +1,14 @@
+export class Project{
+    name:string;
+    description:string;
+    url:string;
+
+    constructor(name:string,description:string,url:string){
+        this.name=name;
+        this.description=description;
+        this.url=url;
+    }
+
+}
+
+
