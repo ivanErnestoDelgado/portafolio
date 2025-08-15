d017e5afbb7f3277566c7a8dcdf836449afe66ee
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-08-14 18:39:52 -0700

    A file with array of instances of projects  simulating API call added

---
+++ src/db/projectsApiCall.ts
@@ -0,0 +1,10 @@
+import { Project } from "../Models";
+
+//simulando una API Call xdxdxdxd
+export const projects=[
+      new Project("Task Manager", "Una API rest de un gestor de tareas hecha con Express.js","https://github.com/ivanErnestoDelgado/TaskProjectBackend"),
+      new Project("Michican","Una pagina web de una veterinaria diseñada en JSP","https://github.com/ivanErnestoDelgado/Michican"),
+      new Project("GrowSmart API","Una API rest hecha en Django para el control de datos de riego de diferentes macetas","https://github.com/ivanErnestoDelgado/GrowSmartServidor"),
+      new Project("Loco Momo","Un proyecto en C# que consiste en recrear el juego de mesa Loco Momo","https://github.com/ivanErnestoDelgado/LocoMomo")
+    ];
+    
